092672041c44c2deb3f011b45bd309f7e6b0d48b
Alejandro Fernández Gete <22171935@live.uem.es>
2022-03-22 17:47:44 +0100

    Cositas

--- src/aplication/Main.java
+++ src/aplication/Main.java
@@ -1,5 +1,12 @@
 package aplication;
+import java.io.IOException;
+import control.controlLogin;
 
 public class Main{
-
+	
+	public static void main(String[] args) {
+		System.out.println("xd");
+		controlLogin control = new controlLogin();
+		control.
+}
 }
--- src/control/controlLogin.java
+++ src/control/controlLogin.java
@@ -1,5 +1,32 @@
 package control;
 
+import aplication.ficheros;
+import model.Usuario;
 public class controlLogin {
+	
+	private String userNameText;
 
+	private String passwordText;
+	
+	void comprobarLogin() {
+		ficheros files = new ficheros();
+		Usuario usuario = files.IniciarSesion(userNameText, passwordText);
+		if(usuario!=null) {
+			String rol = usuario.getUserType();
+			switch (rol) {
+			case "administrador":
+				System.out.println("Es un admin");
+				break;
+			case "jugador":
+				System.out.println("Es un jugador");
+
+				break;
+			
+			}
+		}else {
+			System.out.println("ERROR");
+		}
+
+
+	}
 }
