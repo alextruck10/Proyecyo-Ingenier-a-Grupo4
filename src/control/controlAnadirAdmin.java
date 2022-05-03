50d0910af78657882b9b050055cce7d8bb9f576d
Alejandro Fernández Gete <22171935@live.uem.es>
2022-05-03 11:08:32 +0200

    Añadir admin

--- src/control/controlAnadirAdmin.java
+++ src/control/controlAnadirAdmin.java
@@ -1,10 +1,82 @@
 package control;
 import model.Administrador;
-public class controlAnadirAdmin {
 
+import java.util.Scanner;
+
+import aplication.ficheros;
+import model.Administrador;
+import model.Jugador;
+import model.Usuario;
+
+public class controlAnadirAdmin {
+	private String userId;
+	private String email;
+	private String password;
+	private String userType = "administrador";
+	private String name;
+	private String lastnames;
+	private String birthday;
+	private String genre;
+	private boolean active=true;
+	private String opciongenero;
+	private String opcionactivo;
+	private Administrador user;
+	
 	public void AnadirAdmin()	
 	{
-		System.out.println("Has elegido CREAR UN ADMIN.");
+		System.out.println("Has elegido CREAR UN ADMINISTRADOR.");
+		System.out.println("1.Email:");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Email: ");
+		email = sc.nextLine();
+		System.out.println("Password: ");
+		password = sc.nextLine();
+		System.out.println("UserID: ");
+		userId = sc.nextLine();
+		System.out.println("Name: ");
+		name = sc.nextLine();
+		System.out.println("LastName: ");
+		lastnames = sc.nextLine();
+		System.out.println("Birthday DD-MM-AAAA: ");
+		birthday = sc.nextLine();
+		System.out.println("Genre: (hombre/mujer)");
+		genre = sc.nextLine();
+		System.out.println("Usuario activo? (y/n)");
+		opcionactivo = sc.nextLine();
+		
+		
+		if (opcionactivo == "y") {
+			active = true;
+		} else if (opcionactivo == "n") {
+			active = false;
+		}
+		System.out.println("PERFECTO. Ya tenemos todo.");
+		System.out.println("       ");
+		System.out.println("       ");
+		System.out.println("Resumen del usuario a crear:");
+		System.out.println("Nombre:" + name);
+		System.out.println("Apellidos:" + lastnames);
+		System.out.println("Email:" + email);
+		System.out.println("Password:" + password);
+		System.out.println("Cumpleaños:" + birthday);
+		System.out.println("ID:" + userId);
+		System.out.println("Genero:" + genre);
+		System.out.println("Activo?:" + active);
+
+
+    	Usuario nuevo = new Usuario(userId, email, password, userType);
+    	Jugador nuevo2 = new Jugador (userId, email, password, userType, name, lastnames, birthday, genre, active);
+    	
+    	System.out.println("El usuario que se va a anadir es: " + nuevo2.toString());
+    	String ruta = "src/files/administradores/" + userId + ".jsonl";
+    	
+    	ficheros fichero = new ficheros();
+    	fichero.escribirLogin(nuevo);
+    	fichero.escribirPersona(nuevo2, ruta);
+	}
+	
+	public void setUsuario(Administrador user) {
+		this.user = user;
 	}
 	
 	
