6b0160fea8e045c588db51e673ac5ae20e6a8815
Alejandro Fernández Gete <22171935@live.uem.es>
2022-05-03 12:53:24 +0200

    cositas

--- src/control/controlModificarJugador.java
+++ src/control/controlModificarJugador.java
@@ -2,50 +2,78 @@ package control;
 
 import java.util.Scanner;
 
+import aplication.ficheros;
 import model.Administrador;
+import model.Jugador;
+import model.Usuario;
 
 public class controlModificarJugador extends controlModificarUsuario{
 	private Administrador user;
 	private int opcion;
+	private String userId;
+	private String email;
+	private String password;
+	private String userType = "administrador";
+	private String name;
+	private String lastnames;
+	private String birthday;
+	private String genre;
+	private boolean active=true;
+	private String opciongenero;
+	private String opcionactivo;
 
 
 	void modificarJugador() {
 		System.out.println("TE HAS ADENTRADO EN EL SISTEMA PARA MODIFICAR UN JUGADOR");		
 		user.borrarUsuario(idText);
+		System.out.println("1.Email:");
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Elija una de las opciones 1-9: \n");
-		System.out.println("1.userId\n");
-		System.out.println("2.email\n");
-		System.out.println("3.password\n");
-		System.out.println("4.userType\n");
-		System.out.println("5.name\n");
-		System.out.println("6.lastnames\n");
-		System.out.println("7.birthday\n");
-		System.out.println("8.genre\n");
-		System.out.println("9.active\n");
+		System.out.println("Email: ");
+		email = sc.nextLine();
+		System.out.println("Password: ");
+		password = sc.nextLine();
+		System.out.println("UserID: ");
+		userId = sc.nextLine();
+		System.out.println("Name: ");
+		name = sc.nextLine();
+		System.out.println("LastName: ");
+		lastnames = sc.nextLine();
+		System.out.println("Birthday DD-MM-AAAA: ");
+		birthday = sc.nextLine();
+		System.out.println("Genre: (hombre/mujer)");
+		genre = sc.nextLine();
+		System.out.println("Usuario activo? (y/n)");
+		opcionactivo = sc.nextLine();
 		
-		opcion=sc.nextInt();
 		
-		switch(opcion) {
-		case 1:
-			break;
-		case 2:
-			break;
-		case 3:
-			break;
-		case 4:
-			break;
-		case 5:
-			break;
-		case 6:
-			break;
-		case 7:
-			break;
-		case 8:
-			break;
-		case 9:
-			break;
+		if (opcionactivo == "y") {
+			active = true;
+		} else if (opcionactivo == "n") {
+			active = false;
 		}
+		System.out.println("PERFECTO. Ya tenemos todo.");
+		System.out.println("       ");
+		System.out.println("       ");
+		System.out.println("Resumen del usuario a crear:");
+		System.out.println("Nombre:" + name);
+		System.out.println("Apellidos:" + lastnames);
+		System.out.println("Email:" + email);
+		System.out.println("Password:" + password);
+		System.out.println("Cumpleaños:" + birthday);
+		System.out.println("ID:" + userId);
+		System.out.println("Genero:" + genre);
+		System.out.println("Activo?:" + active);
+
+
+    	Usuario nuevo = new Usuario(userId, email, password, userType);
+    	Jugador nuevo2 = new Jugador (userId, email, password, userType, name, lastnames, birthday, genre, active);
+    	
+    	System.out.println("El usuario que se va modificar es: " + nuevo2.toString());
+    	String ruta = "src/files/jugadores/" + userId + ".jsonl";
+    	
+    	ficheros fichero = new ficheros();
+    	fichero.escribirLogin(nuevo);
+    	fichero.escribirPersona(nuevo2, ruta);
 	}
 	
 	public void setUser(Administrador user) {
