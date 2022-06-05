692ffe73ad4e1b88b9409625afd8ceeaa3eb1bd0
sysialvarez <ismacorporation@gmail.com>
2022-06-05 20:46:50 +0200

    Subimos test mail

--- src/aplication/Main.java
+++ src/aplication/Main.java
@@ -1,13 +1,15 @@
 package aplication;
 import java.io.IOException;
+
 import java.util.*;
+import javax.activation.*;
 import control.controlLogin;
 
 public class Main{
 	
 	public static void main(String[] args) throws IOException {
 		
-			controlLogin control = new controlLogin(); 
+        controlLogin control = new controlLogin(); 
 		control.comprobarLogin();
 		
 		
--- src/control/controlLogin.java
+++ src/control/controlLogin.java
@@ -6,13 +6,37 @@ import java.io.IOException;
 
 import aplication.ficheros;
 import model.Usuario;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.activation.*;
 public class controlLogin {
 	 
 	private String userNameText;
 
 	private String passwordText;
 	private Boolean entra=true;
+	private final Properties properties = new Properties();
+	
+	private String password;
+ 
+	private Session session;
 
+	private void init() {
+		 
+		properties.put("mail.smtp.host", "mail.gmail.com");
+		properties.put("mail.smtp.starttls.enable", "true");
+		properties.put("mail.smtp.port",25);
+		properties.put("mail.smtp.mail.sender","tributestats@gmail.com");
+		properties.put("mail.smtp.user", "tributestats@gmail.com");
+		properties.put("mail.smtp.auth", "true");
+		
+		session = Session.getDefaultInstance(properties);
+	}
+	
 	public void comprobarLogin() throws IOException {
 		try
 		{
@@ -37,6 +61,7 @@ public class controlLogin {
 		Usuario usuario = files.IniciarSesion(userNameText, passwordText);
 		if(usuario!=null) {
 			String rol = usuario.getUserType();
+			//sendEmail();
 			switch (rol) {
 			case "administrador":
 				if(entra.equals(usuario.active)) {
@@ -44,6 +69,7 @@ public class controlLogin {
 				controlAdmin controlAdmin1 = new controlAdmin();
 				controlAdmin1.setUsuario(files.leerAdministrador("src/files/administradores/" + usuario.getUserId() + ".jsonl"));
 				controlAdmin1.menuAdmin();
+				
 				}else {
 					System.out.println("SU CUENTA SE ENCUENTRA EN ESTE MOMENTO SUSPENDIDA");
 					return;
@@ -56,7 +82,8 @@ public class controlLogin {
 				System.out.println("Bienvenido " + usuario.getName());
 				controlJugador controlJugador1 = new controlJugador();
 				controlJugador1.setUser(files.leerJugador("src/files/jugadores/" + usuario.getUserId() + ".jsonl"));
-				controlJugador1.menuJugador();				
+				controlJugador1.menuJugador();		
+				
 				}else {
 					System.out.println("SU CUENTA SE ENCUENTRA EN ESTE MOMENTO SUSPENDIDA");
 					return;
@@ -71,4 +98,25 @@ public class controlLogin {
 
 
 	}
+	public void sendEmail() {
+		 
+		init();
+		try{
+			MimeMessage message = new MimeMessage(session);
+			message.setFrom(new InternetAddress((String)properties.get("mail.smtp.mail.sender")));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress("ismacorporation@gmail.com"));
+			message.setSubject("Prueba");
+			message.setText("Texto");
+			Transport t = session.getTransport("smtp");
+			t.connect((String)properties.get("tributestats@gmail.com"), "Pass_1234");
+			t.sendMessage(message, message.getAllRecipients());
+			t.close();
+		}catch (MessagingException me){
+                        //Aqui se deberia o mostrar un mensaje de error o en lugar
+                        //de no hacer nada con la excepcion, lanzarla para que el modulo
+                        //superior la capture y avise al usuario con un popup, por ejemplo.
+			return;
+		}
+		
+	} 
 }
